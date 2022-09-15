260a19f834682c248408057a4ba2e919c193341c
Lakoc <xpolok03@stud.fit.vutbr.cz>
2022-09-15 12:57:24 +0200

    therapist, client rename

--- VBx/vbhmm_tid.py
+++ VBx/vbhmm_tid.py
@@ -393,7 +393,7 @@ if __name__ == '__main__':
             write_output(fp, out_labels, starts, ends)
 
         segments, _ = load_diar_rttm_unchanged(rttm_path)
-        speaker_tags = ['terapeut', 'klient']
+        speaker_tags = ['therapist', 'client']
         segments = [normalize_tuple_s([segment[1], segment[2] - segment[1], f'{speaker_tags[segment[0] - 1]}'])
                     for segment in segments]
         arr_to_ctm(args.out_ctm, segments)
